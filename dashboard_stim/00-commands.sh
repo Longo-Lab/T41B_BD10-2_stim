de7ac1a476733ebf0554a463d030e97290470af7
Robert Raymond Butler III <6644896+rbutleriii@users.noreply.github.com>
2023-08-01 00:07:01 -0700

    202308010000 backup

---
+++ dashboard_stim/00-commands.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+round_num="R1"
+nameset="T41B_stim"
+
+# T41B_stim (data files obtained from parent directory)
+mkdir -p $nameset
+gzip -c ../res.5.csv > "${nameset}/${round_num}.${nameset}.RNA.Wt-StvsWt-Un.csv.gz"
+# gzip -c ../res.X.csv > "${nameset}/${round_num}.${nameset}.RNA.Wt-D-StvsWt-D-Un.csv.gz"
+gzip -c ../res.6.csv > "${nameset}/${round_num}.${nameset}.RNA.Tg-StvsTg-Un.csv.gz"
+gzip -c ../res.7.csv > "${nameset}/${round_num}.${nameset}.RNA.Tg-D-StvsTg-D-Un.csv.gz"
+
+# biodomain_correlation.R
+sbatch -J $nameset --mem=5G -c 2 -t 01:00:00 -p interactive \
+  -o %x/%A_biodomain_correlation_%x.log \
+  --wrap "ml R/4.2.2; biodomain_correlation.R -n $nameset -g APPL/S -d BD10-2 -c Gene_id,log2FoldChange -a Wt-StvsWt-Un,Tg-StvsTg-Un,Tg-D-StvsTg-D-Un -t stim"
+
+# biodomain_enrichment.R
+sbatch -J $nameset --mem=5G -c 2 -t 01:00:00 -p interactive \
+  -o %x/%A_biodomain_enrichment_%x.log \
+  --wrap "ml R/4.2.2; biodomain_enrichment.R -n $nameset -g APPL/S -d BD10-2 -c Gene_id,log2FoldChange,padj -a Wt-StvsWt-Un,Tg-StvsTg-Un,Tg-D-StvsTg-D-Un -t stim"
+
+# save_dashboard_files.R
+sbatch -J $nameset --mem=5G -c 2 -t 01:00:00 -p interactive \
+  -o %x/%A_save_dashboard_files_%x.log \
+  --wrap "ml R/4.2.2; save_dashboard_files.R $nameset"
