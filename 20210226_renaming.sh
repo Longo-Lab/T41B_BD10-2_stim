bd1ffdda6ee517ebe039e9e8d8677b408b71e25c
Robert Raymond Butler III <6644896+rbutleriii@users.noreply.github.com>
2023-09-20 11:33:59 -0700

    updates for biorxiv

---
+++ 20210226_renaming.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+mv drug_effect_across_stimulations_annotated.csv res.10.csv
+mv results_drugEffect_Stim_annotated.csv res.9.csv
+mv results_TG_drug_effect_noSTIM_annotated.csv res.8.csv
+mv results_TG_VEH_STIM_vsTG_VEH_NOSTIM_annotated.csv res.6.csv
+mv results_WT_VEH_STIM_vsWT_VEH_NOSTIM_annotated.csv res.5.csv
+mv results_TG_VEHvsWT_VEH_STIM_annotated.csv res.2.csv
+mv results_TG_VEHvsWT_VEH_noSTIM_annotated.csv res.1.csv
+mv results_TG_BD10_LTP_vsWT_VEH_STIM_annotated.csv res.12.csv
+mv results_TG_veh_LTP_vsWT_VEH_noSTIM_annotated.csv res.14.csv
+mv results_TG_BD10noLTP_vsWT_VEH_noSTIM_annotated.csv res.11.csv
+mv results_TG_DRUG_STIM_vsTG_DRUG_NOSTIM_annotated.csv res.7.csv
+
+mv BD10_GO_Term_BP_MF_CC_KEGG_REAC_Drug_effect_acrossStimulations_SignificanceAll.csv gost.10.csv
+mv BD10_GO_Term_BP_MF_CC_KEGG_REAC_Drug_effect_in_TG_LTP_withInteractionTerm_SignificanceAll.csv gost.9.csv
+mv BD10_GO_Term_BP_MF_CC_KEGG_REAC_TG_DRUG_effect_pvalue0.05_SignificanceAll.csv gost.8.csv
+mv BD10_GO_Term_BP_MF_CC_KEGG_REAC_TG_VEH_LTP_vs_TG_VEH_noLTP_DGE_pvalue0.05_SignificanceAll.csv gost.6.csv
+mv BD10_GO_Term_BP_MF_CC_KEGG_REAC_WT_VEH_LTP_vs_WT_VEH_noLTP_DGE_pvalue0.05_SignificanceAll.csv gost.5.csv
+mv 2.BD10_GO_Term_BP_MF_CC_KEGG_REAC_TG_VEH_WT_VEH_LTP_DGE_pvalue0.05_SignificanceAll.csv gost.2.csv
+mv 1.BD10_GO_Term_BP_MF_CC_KEGG_REAC_TG_VEH_WT_VEH_noLTP_DGE_pvalue0.05_SignificanceAll.csv gost.1.csv
+mv BD10_GO_Term_BP_MF_CC_KEGG_REAC_Drug_TG_BD10-2_vs_WT_Veh_LTP.csv gost.12.csv
+
