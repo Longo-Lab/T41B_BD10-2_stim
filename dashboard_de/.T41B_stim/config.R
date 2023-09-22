49039967f942479a9678c26bfc0ee55e02e6138b
Robert Raymond Butler III <6644896+rbutleriii@users.noreply.github.com>
2023-09-22 00:06:39 -0700

    202309220000 backup

--- dashboard_de/.T41B_stim/config.R
+++
@@ -1,6 +0,0 @@
-drug <- 'TBS'
-analyses <- c('Tg-VvsWt-V', 'Tg-DvsWt-V', 'Tg-DvsTg-V', 'Wt-DvsWt-V')
-de_cols <- c('Gene_id', 'log2FoldChange', 'padj', 'pvalue')
-de_names <- c('Log2FC', 'Pval (adj)', 'Pval')
-lfc_cutoff_geno <- 3
-lfc_cutoff_drug <- 5
