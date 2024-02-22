4c50b882921c868badaa0294fcec9b964b4ea2ba
Robert Raymond Butler III <6644896+rbutleriii@users.noreply.github.com>
2024-02-21 16:19:16 -0800

    updated 02 plot

--- enrichment_plots/02-drug_enrich_plot.R
+++ enrichment_plots/02-drug_enrich_plot.R
@@ -3,7 +3,7 @@
 #'
 #' Script name: 02-drug_enrich_plot.R
 #'
-#' Version: 0.0.2
+#' Version: 0.0.3
 #'
 #' Purpose of script: plot top ontology enrichments across three drug conditions
 #'
@@ -33,7 +33,8 @@
 
 library(data.table)
 library(ggplot2)
-
+library(patchwork)
+library(stringr)
 
 # filepaths
 rootdir <- "/labs/flongo/t41b_BD10-2_stim"
@@ -50,109 +51,111 @@ tiss <- data.table(
   "full" = c("APP", "BD10-2", "APP-BD10-2")
 )
 target_files <- unlist(lapply(tiss$short, function(i) {
-  c(paste0(rootdir, paste0("/gost.padj.", i, ".csv.gz")))
+  c(paste0(rootdir, paste0("/gost.", i, ".csv")))
 }))
 if (!all(file.exists(target_files))) {
   stop("One or more input files are missing .n", call. = FALSE)
 }
 
 
-# Load data ------
+# Load data --------------------------------------------
 fl <- lapply(target_files, fread)
 setattr(fl, "names", tiss$full)
 fd <- rbindlist(fl, use.names = T, idcol = "Tiss")
 
 # clean up for parsing --------------------------------
-fd[, c("intersection", "evidence_codes", "symbols", "source_order", "parents", "precision", "recall", "significant", "effective_domain_size", "Gene_name") := NULL]
+drops = c(
+  "intersection", 
+  "evidence_codes", 
+  "source_order", 
+  "precision", 
+  "recall", 
+  "significant", 
+  "effective_domain_size", 
+  "Gene_name"
+) 
+fd[, (drops) := NULL]
 fd[, LogBH := -log10(p_value)]
-fd[query == "downregulated", LogBH := -(LogBH)]
+fd[DGE == "downregulated", LogBH := -(LogBH)]
 fd <- fd[term_size < 1000]
-# cats = c("GO:BP", "GO:CC", "KEGG", "REAC", "WP")
-# cats = c("GO:BP")
-# fd = fd[source %in% cats]
-# plt = fd[, head(.SD, 10), by=c("DGE", "Tiss")][, term_name]
-# plt = fd[Tiss == "Wt", head(.SD, 5), by=c("source", "query")][,term_name]
+
+# prepping terms of interest ---------------------------
 plt <- c(
-  "cytokine production",
+  "cytokine production", 
   "inflammatory response",
-  "cytokine receptor activity",
   "regulation of neuron death",
-  "intrinsic apoptotic signaling pathway",
+  "intrinsic apoptotic signaling pathway", 
+  "cytokine receptor activity", 
   "neuroinflammatory response",
-  "complement activation",
-  "cell aging",
-  "Alzheimers Disease",
+  "Complement cascade",
+  "cell aging", 
+  "Alzheimers Disease", 
   "MAP kinase activation",
-  "positive regulation of amyloid-beta formation",
+  "regulation of amyloid-beta formation",
+  "regulation of neurogenesis",
+  "cell projection membrane",
   "axoneme",
+  "regulation of ERK1 and ERK2 cascade",
+  "positive regulation of ERK1 and ERK2 cascade", 
+  "negative regulation of ERK1 and ERK2 cascade",
+  "PI3K-Akt signaling pathway",
+  "JNK cascade",
+  "regulation of Wnt signaling pathway",
+  "positive regulation of Wnt signaling pathway",
+  "negative regulation of Wnt signaling pathway",
   "regulation of long-term synaptic potentiation",
   "long-term synaptic potentiation",
   "glutamate receptor activity",
-  "EEG with spike-wave complexes",
+  "EEG with spike-wave complexes", 
   "EEG abnormality",
   "EEG with generalized epileptiform discharges",
-  "vesicle-mediated transport in synapse",
+  "vesicle-mediated transport in synapse", 
   "signal release from synapse",
   "neuronal action potential",
-  "Glutamatergic synapse",
-  "GABA-ergic synapse",
+  "Glutamatergic synapse", 
+  "GABA-ergic synapse", 
   "postsynaptic membrane",
-  "synaptic signaling",
-  "postsynapse",
-  "presynapse",
-  "Complement cascade",
-  "Complement activation, classical pathway",
-  "PI3K-Akt signaling pathway",
-  "Focal adhesion: PI3K-Akt-mTOR signaling pathway",
-  "JNK cascade",
-  "ERK1 and ERK2 cascade",
-  "Wnt signaling pathway and pluripotency",
-  "Wnt signaling pathway"
+  "synaptic signaling", 
+  "postsynapse", 
+  "presynapse"
 )
+
+# Build short list
 a <- fd[term_name %in% plt]
-# # shuffle terms so microtubule based processes isn't at the bottom
-# term_order = rev(unique(a$term_name))
-# term_order = append(term_order[-1], term_order[1], after=11)
+
+# Make terms sentence case with abbreviations
+abbr <- c("ERK", "Wnt", "MAP", "JNK", "PI3K-Akt", "EEG", "GABA")
+repl <- rep(abbr, 2)
+names(repl) <- c(str_to_sentence(abbr), str_to_lower(abbr))
+plt <- str_replace_all(str_to_sentence(plt), repl)
+a$term_name <- str_replace_all(str_to_sentence(a$term_name), repl)
+
+# Assign factors for ordering of plots
 a$term_name <- factor(a$term_name, levels = plt)
 a$Tiss <- factor(a$Tiss, levels = rev(tiss$full))
 
-# fill in missing values so plot works ------------------
-# a = rbindlist(list(a, data.table(
-# "Tiss"=tiss$full[2],
-# "LogBH"=0,
-# "term_name"=term_order[23]
-# )), fill=T)
-# a = rbindlist(list(a, data.table(
-# "Tiss"=rep(tiss$full[2], length(plt[c(15, 21, 23)])),
-# "LogBH"=rep(0, length(plt[c(15, 21, 23)])),
-# "term_name"=plt[c(15, 21, 23)]
-# )), fill=T)
-# a = rbindlist(list(a, data.table(
-# "Tiss"=tiss$full[3],
-# "LogBH"=0,
-# "term_name"=term_order[12]
-# )), fill=T)
+# fill in missing values so plot works 
+miss <- setdiff(plt, a[LogBH <=0 & Tiss == tiss$full[2], term_name])
+a <- rbindlist(list(a, 
+  data.table(
+    "Tiss" = rep(tiss$full[2], length(miss)),
+    "LogBH" = rep(0, length(miss)),
+    "term_name" = miss
+  )
+), fill = TRUE)
 
 
 # Plot -----------------------------------------------
-p <- ggplot(a, aes(x = LogBH, y = term_name, fill = Tiss)) +
-  geom_bar(stat = "identity", position = "dodge") +
+a %>% 
+  ggplot(aes(x = LogBH, y = term_name, fill = Tiss)) +
+  geom_bar(stat = "identity", position = "dodge", width = 0.7) +
   guides(fill = guide_legend(reverse = TRUE)) +
   geom_vline(xintercept = 0) +
   scale_fill_brewer(type = "qual", palette = "Set2") +
   xlab(expression("Downregulated" %<-% "-Log10 FDR" %->% "Upregulated")) +
   geom_text(
-    data = subset(a, LogBH <= 0 & Tiss == tiss$full[3]),
-    aes(0, y = term_name, label = term_name),
-    hjust = 0,
-    nudge_x = 10,
-    colour = "black",
-    family = "Arial",
-    size = 4
-  ) +
-  geom_text(
-    data = subset(a, LogBH > 0 & Tiss == tiss$full[3]),
-    aes(0, y = term_name, label = term_name),
+    data = a[LogBH <= 0 & Tiss == tiss$full[2]],
+    aes(0, y=term_name, label=term_name),
     hjust = 0,
     nudge_x = 10,
     colour = "black",
@@ -161,7 +164,7 @@ p <- ggplot(a, aes(x = LogBH, y = term_name, fill = Tiss)) +
   ) +
   theme(
     panel.background = element_rect(fill = "white"),
-    panel.grid.major.x = element_line(color = "#A8BAC4", size = 0.3),
+    panel.grid.major.x = element_line(color = "#A8BAC4", linewidth = 0.3),
     axis.ticks.length = unit(0, "mm"),
     axis.title.y = element_blank(),
     axis.title.x = element_text(family = "Arial", size = 16, face = "bold"),
@@ -171,8 +174,14 @@ p <- ggplot(a, aes(x = LogBH, y = term_name, fill = Tiss)) +
     plot.title = element_text(size = 16, hjust = 0.5, face = "bold"),
     plot.subtitle = element_text(size = 18, hjust = 0.5)
   ) +
-  ggtitle("Selected LTP-relevant Term Enrichment")
+  ggtitle("Selected LTP-relevant Term Enrichment") -> p1
+
 ggsave(
   filename = paste(today, nameset, "drug_enrich_bar.pdf", sep = "."),
-  plot = p, device = cairo_pdf, width = 13, height = 12
+  plot = p1, 
+  device = cairo_pdf, 
+  width = 13, 
+  height = 18
 )
+
+
